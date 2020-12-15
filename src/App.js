72407e71187aaba4a8e74c8c7c8179ba37625fe6
İsa Kulaksız <isakulaksiz@outlook.com>
2020-12-15 12:09:44 +0300

    remove favourite from state

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import MovieList from './components/movielist';
 import MovieListHeading from './components/movielistheading';
 import SearchBox from './components/searchbox';
 import AddFavourites from './components/addfavourites';
+import RemoveFavourites from './components/remotefavourites';
 
 const App = () => {
   const [movies, setMovies] = useState([]);
@@ -27,6 +28,12 @@ const App = () => {
     setFavourites(newFavourList);
   }
 
+  const removeFavouriteMovie = (movie) => {
+    const newFavourList = favourites.filter((favourite) => favourite.imdbID !== movie.imdbID);
+
+    setFavourites(newFavourList);
+  }
+
   useEffect(() => {
     getMovieRequest(searchValue);
   }, [searchValue]);
@@ -44,7 +51,7 @@ const App = () => {
 				<MovieListHeading heading='Favourites' />
 			</div>
 			<div className='row'>
-				<MovieList movies={favourites} favouriteComponent={AddFavourites} />
+				<MovieList movies={favourites} favouriteComponent={RemoveFavourites} handleFavouritesClick={removeFavouriteMovie}/>
 			</div>
     </div>
   )
